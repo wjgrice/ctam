eb307ef3082a9e940ea2c6842f6ff84f65235224
Jeremy <wjgrice@gmail.com>
2023-05-17 12:12:19 -0500

    Project folder established

---
+++ backend/src/main/java/net/minminder/ctam/CtamApplication.java
@@ -0,0 +1,13 @@
+package net.minminder.ctam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtamApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/net/minminder/ctam/CtamApplicationTests.java
@@ -0,0 +1,13 @@
+package net.minminder.ctam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
